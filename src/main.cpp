4defa35e8fb1657973d228502e634a4181fd990b
Saeed Ghaffari <gh.msaeed@gmail.com>
2023-01-12 17:10:07 +0330

    Feat: implement redirection

--- src/main.cpp
+++ src/main.cpp
@@ -11,86 +11,73 @@
 #include "http_server.cpp"
 #include "uri.h"
 
-using simple_http_server::HttpMethod;
-using simple_http_server::HttpRequest;
-using simple_http_server::HttpResponse;
-using simple_http_server::HttpServer;
-using simple_http_server::HttpStatusCode;
+using namespace std;
 
-void ensure_enough_resource(int resource, std::uint32_t soft_limit,
-                            std::uint32_t hard_limit) {
+void ensure_enough_resource(int resource, uint32_t soft_limit,
+                            uint32_t hard_limit) {
     rlimit new_limit, old_limit;
 
     new_limit.rlim_cur = soft_limit;
     new_limit.rlim_max = hard_limit;
     getrlimit(resource, &old_limit);
 
-    std::cout << "Old limit: " << old_limit.rlim_cur << " (soft limit), "
-              << old_limit.rlim_cur << " (hard limit)." << std::endl;
-    std::cout << "New limit: " << new_limit.rlim_cur << " (soft limit), "
-              << new_limit.rlim_cur << " (hard limit)." << std::endl;
+    cout << "Old limit: " << old_limit.rlim_cur << " (soft limit), "
+              << old_limit.rlim_cur << " (hard limit)." << endl;
+    cout << "New limit: " << new_limit.rlim_cur << " (soft limit), "
+              << new_limit.rlim_cur << " (hard limit)." << endl;
 
     if (setrlimit(resource, &new_limit)) {
-        std::cerr << "Warning: Could not update resource limit ";
-        std::cerr << "(" << strerror(errno) << ")." << std::endl;
-        std::cerr << "Consider setting the limit manually with ulimit" << std::endl;
+        cerr << "Warning: Could not update resource limit ";
+        cerr << "(" << strerror(errno) << ")." << endl;
+        cerr << "Consider setting the limit manually with ulimit" << endl;
         exit(-1);
     }
 }
 
-int main(void) {
-    std::string host = "0.0.0.0";
-    int port = 8080;
+int main(int argc, char **argv) {
+    string host = "0.0.0.0";
+    int port = stoi(argv[1]);
     HttpServer server(host, port);
-
     // Register a few endpoints for demo and benchmarking
-    auto say_hello = [](const HttpRequest &request) -> HttpResponse {
-        HttpResponse response(HttpStatusCode::Ok);
-        response.SetHeader("Content-Type", "text/plain");
-        response.SetContent("Hello, world\n");
+    auto redirectToIndex = [](const HttpRequest &request) -> HttpResponse {
+        HttpResponse response(HttpStatusCode::MovedPermanently);
+        response.SetHeader("Location", "index.html");
         return response;
     };
-    auto send_html = [](const HttpRequest &request) -> HttpResponse {
+    auto index = [](const HttpRequest &request) -> HttpResponse {
         HttpResponse response(HttpStatusCode::Ok);
-        std::string content;
+        string content;
         content += "<!doctype html>\n";
         content += "<html>\n<body>\n\n";
         content += "<h1>Hello, world in an Html page</h1>\n";
         content += "<p>A Paragraph</p>\n\n";
         content += "</body>\n</html>\n";
-
         response.SetHeader("Content-Type", "text/html");
         response.SetContent(content);
         return response;
     };
-
-    server.RegisterHttpRequestHandler("/", HttpMethod::HEAD, say_hello);
-    server.RegisterHttpRequestHandler("/", HttpMethod::GET, say_hello);
-    server.RegisterHttpRequestHandler("/hello.html", HttpMethod::HEAD, send_html);
-    server.RegisterHttpRequestHandler("/hello.html", HttpMethod::GET, send_html);
-
+    server.RegisterHttpRequestHandler("/", HttpMethod::HEAD, redirectToIndex);
+    server.RegisterHttpRequestHandler("/", HttpMethod::GET, redirectToIndex);
+    server.RegisterHttpRequestHandler("/index.html", HttpMethod::HEAD, index);
+    server.RegisterHttpRequestHandler("/index.html", HttpMethod::GET, index);
     try {
-        // std::cout << "Setting new limits for file descriptor count.." <<
-        // std::endl; ensure_enough_resource(RLIMIT_NOFILE, 15000, 15000);
-
-        // std::cout << "Setting new limits for number of threads.." << std::endl;
+        // cout << "Setting new limits for file descriptor count.." <<
+        // endl; ensure_enough_resource(RLIMIT_NOFILE, 15000, 15000);
+        // cout << "Setting new limits for number of threads.." << endl;
         // ensure_enough_resource(RLIMIT_NPROC, 60000, 60000);
-
-        std::cout << "Starting the web server.." << std::endl;
+        cout << "Starting the web server.." << endl;
         server.Start();
-        std::cout << "Server listening on " << host << ":" << port << std::endl;
-
-        std::cout << "Enter [quit] to stop the server" << std::endl;
-        std::string command;
-        while (std::cin >> command, command != "quit");
-        std::cout << "'quit' command entered. Stopping the web server.."
-                  << std::endl;
+        cout << "Server listening on " << host << ":" << port << endl;
+        cout << "Enter [quit] to stop the server" << endl;
+        string command;
+        while (cin >> command, command != "quit");
+        cout << "'quit' command entered. Stopping the web server.."
+                  << endl;
         server.Stop();
-        std::cout << "Server stopped" << std::endl;
-    } catch (std::exception &e) {
-        std::cerr << "An error occurred: " << e.what() << std::endl;
+        cout << "Server stopped" << endl;
+    } catch (exception &e) {
+        cerr << "An error occurred: " << e.what() << endl;
         return -1;
     }
-
     return 0;
 }
